cd01142afe340a24f034f2bdbc3e4bf00eef1ef2
Iuri Moro <iuribpmoro@gmail.com>
2022-11-14 20:04:50 -0300

    Removed logout from home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Title from "../../components/Title";
-import { auth } from "../../hooks/auth";
 
 const Home: React.FC = () => {
   return (
     <div>
       <Title>Home</Title>
-      <button onClick={() => auth.signOut()}>Sign out</button>
     </div>
   );
 }
