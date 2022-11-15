ea32d8a9ea070bbfe640cfcb562684bafbfb5691
Iuri Moro <iuribpmoro@gmail.com>
2022-11-14 22:04:51 -0300

    Added register opportunity button to home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,14 @@
-import { Card, CardContent } from "@mui/material";
+import { Button, Card, CardContent } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Title from "../../components/Title";
 import { getOpportunities } from "../../hooks/opportunities";
 import IOpportunity from "../../interfaces/IOpportunity";
+import { Colors } from "../../utils/colors";
 
 const Home: React.FC = () => {
   const [opportunities, setOpportunities] = useState<IOpportunity[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     getOpportunities().then((response) => {
@@ -17,6 +20,14 @@ const Home: React.FC = () => {
   return (
     <div>
       <Title>Vagas Disponíveis</Title>
+
+      <Button
+        variant="contained"
+        sx={{ borderRadius: 25, backgroundColor: Colors.primary }}
+        onClick={() => navigate('/register-opportunity')}
+      >
+        Adicionar vaga
+      </Button>
 
       {opportunities.map((opportunity) => (
         <Card key={opportunity.id}>
